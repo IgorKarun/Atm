2b550a597f018946c416d824af86052f811325db
Igor Karun <iKar@Tron.local>
2015-11-05 10:29:51 +0100

    [ADD] Main DB functionality

---
+++ app/src/main/java/com/ikar/grossumtesttask/db/DbHelper.java
@@ -0,0 +1,41 @@
+package com.ikar.grossumtesttask.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by iKar on 11/5/15.
+ */
+public class DbHelper extends SQLiteOpenHelper{
+
+    static final String DB_NAME = "cashdesk_db";
+    static final int DB_VERSION = 1;
+
+    public static final String TABLE = "cashdesk";
+
+    public static final String _ID = "_id";
+    public static final String _DENOMINATION= "denomination";
+    public static final String _INVENTORY= "inventory";
+
+    static final String DB_CREATE = "create table "
+            + TABLE + "("
+            + _ID + " integer primary key autoincrement, "
+            + _DENOMINATION + " text, "
+            + _INVENTORY + " text"
+            + ");";
+
+    public DbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
